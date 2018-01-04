5bc4858fc4fdd5ae1c219f7b074b14f83cd1c20d
José Simões <josesimoes@users.noreply.github.com>
2018-01-04 08:25:13 +0000

    Revert DateTime package (now it's back in mscorlib) (#4)

--- source/ExecutionConstraint.cs
+++ source/ExecutionConstraint.cs
@@ -13,7 +13,7 @@ namespace nanoFramework.Runtime.Native
     /// The exception that is thrown when an action is attempted that violates a constraint. 
     /// </summary>
     /// <remarks>When a ConstraintException exception is caught, if the ExecutionConstraint object that caused this exception has not been uninstalled, the catch block implementation has a small additional amount of time during which to handle the exception. 
-    /// If this takes too long, the exception is rethrown automatically.
+    /// If this takes too long, the exception is re-thrown automatically.
     /// </remarks>
     public class ConstraintException : Exception
     {
@@ -25,7 +25,7 @@ namespace nanoFramework.Runtime.Native
     public static class ExecutionConstraint
     {
         /// <summary>
-        /// Creates a subthread within the calling thread, containing a constraint that requires the calling thread to complete an operation within a specified time period and at a specified priority level. 
+        /// Creates a sub-thread within the calling thread, containing a constraint that requires the calling thread to complete an operation within a specified time period and at a specified priority level. 
         /// </summary>
         /// <param name="timeout">The number of clock ticks before a <see cref="ConstraintException"/> exception is thrown. Note that the value -1 in this parameter indicates that the current constraint exception is to be uninstalled.</param>
         /// <param name="priority">The priority level of the calling thread.</param>
@@ -34,7 +34,7 @@ namespace nanoFramework.Runtime.Native
         /// </remarks>
         /// <exception cref="Exception">The timeout parameter is less than -1.</exception>
         /// <exception cref="Exception">The system is unable to identify the thread that is installing this constraint.</exception>
-        /// <exception cref="Exception">The thread installing this contraint does not own the subthread that the constraint applies to.</exception>
+        /// <exception cref="Exception">The thread installing this constraint does not own the sub-thread that the constraint applies to.</exception>
         [MethodImpl(MethodImplOptions.InternalCall)]
         extern static public void Install(int timeout, int priority);
     }
