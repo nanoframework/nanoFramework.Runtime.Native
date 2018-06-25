51aad36595ba0f70fefbd2035e5342e5924396c7
José Simões <jose.simoes@eclo.solutions>
2018-06-25 10:22:39 +0100

    Work Native.Rtc class
    - Rename RTC to Rtc (for coherency sake with the rest of the API)
    - Remove SetAlarm method
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- source/RTC.cs
+++ source/RTC.cs
@@ -10,7 +10,7 @@ namespace nanoFramework.Runtime.Native
     /// <summary>
     /// Provides a set of methods that help developers to manage the RTC (Real Time Clock) on the target device.
     /// </summary>
-    public static class RTC
+    public static class Rtc
     {
         /// <summary>
         /// Sets the current system time and date.
@@ -23,25 +23,10 @@ namespace nanoFramework.Runtime.Native
             return Native_RTC_SetSystemTime(time.Year, (byte)time.Month, (byte)time.Day, (byte)time.DayOfWeek, (byte)time.Hour, (byte)time.Minute, (byte)time.Second);
         }
 
-        /// <summary>
-        /// Sets the date and time for the next alarm that will wake-up the processor.
-        /// </summary>
-        /// <param name="time"><see cref="System.DateTime"/> to set.</param>
-        /// <returns>If the function succeeds, the return value is true.</returns>
-        /// <remarks>This method is specific to nanoFramework. The actual availability of the resulting feature depends on the availability on target platform that's running the nanoCLR.</remarks>
-        public static bool SetAlarm(System.DateTime time)
-        {
-            return Native_RTC_SetAlarm(time.Year, (byte)time.Month, (byte)time.Day, (byte)time.DayOfWeek, (byte)time.Hour, (byte)time.Minute, (byte)time.Second);
-        }
-
-
         #region external methods declarations
 
         [MethodImpl(MethodImplOptions.InternalCall)]
         private static extern bool Native_RTC_SetSystemTime(int year, byte month, byte day, byte dayOfWeek, byte hours, byte minutes, byte seconds);
-
-        [MethodImpl(MethodImplOptions.InternalCall)]
-        private static extern bool Native_RTC_SetAlarm(int year, byte month, byte day, byte dayOfWeek, byte hours, byte minutes, byte seconds);
 
         #endregion
     }
