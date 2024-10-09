bbeda14420749f62e1c86ee0028b0eaa8ddb9740
josesimoes <jose.simoes@eclo.solutions>
2024-10-10 00:48:31 +0100

    More code clean-up
    ***NO_CI***

--- nanoFramework.Runtime.Native/Properties/AssemblyInfo.cs
+++ nanoFramework.Runtime.Native/Properties/AssemblyInfo.cs
@@ -1,12 +1,12 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Reflection;
-using System.Runtime.InteropServices;
-
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+using System.Reflection;
+using System.Runtime.InteropServices;
+
 [assembly: AssemblyTitle("nanoFramework.Runtime.Native")]
 [assembly: AssemblyCompany("nanoFramework Contributors")]
 [assembly: AssemblyProduct("nanoFramework.Runtime.Native")]
